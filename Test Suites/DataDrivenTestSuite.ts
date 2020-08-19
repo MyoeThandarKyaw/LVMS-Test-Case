6d55138f3e5a1f829e6a04d0ccf15aa2ad33af4a
MyoeThandarKyaw <yccmyoethandarkyaw7@gmail.com>
2020-08-19 17:11:29 +0630

    Test case for data driven using excel file;

---
+++ Test Suites/DataDrivenTestSuite.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DataDrivenTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ff92c74a-5db9-4358-b34b-0cf2622d8029</testSuiteGuid>
+   <testCaseLink>
+      <guid>362907ba-fc50-429f-95a9-3a0a2fd6e0c6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DataDrivenTestCases/LoginWith_MultipleAccount</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>56c427b5-82df-48c9-8bcb-95b6d8526cde</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/multiple_account_login_data</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>56c427b5-82df-48c9-8bcb-95b6d8526cde</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>be244227-e539-4572-83e8-b48dae3f9ee2</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>56c427b5-82df-48c9-8bcb-95b6d8526cde</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>4cec41b4-dca7-4280-a1ab-4bcf73dbb54b</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
